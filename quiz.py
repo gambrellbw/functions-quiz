8440f6640dcc74863cea16718cae04a260de127e
Brendan Weston Gambrell <gambrellbw@dcsdk12.org>
2016-09-07 12:48:58 -0600

    Implemented two_as_one

--- quiz.py
+++ quiz.py
@@ -41,3 +41,14 @@ print closer_to(21,20,9)#Should return 20
 print closer_to(7,20,9)#Should return 9
 print closer_to(21,9,9)#Should return 0
 print closer_to(17,16,8)#Should return 16
+
+def two_as_one(a,b,c):
+	   if a + b == c or a + c == b or b + c == a:
+			return True
+   else: return False
+
+print two_as_one(1,2,3)#Should return True
+print two_as_one(5,6,11)#Should return True
+print two_as_one(2,2,3)#Should return False
+print two_as_one(3,2,3)#Should return False
+# TODO - write pig_latinif
