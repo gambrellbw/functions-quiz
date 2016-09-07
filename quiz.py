6bd2d2aceadd8abb93610212c3cff21eb5940a43
Brendan Weston Gambrell <gambrellbw@dcsdk12.org>
2016-09-07 12:46:27 -0600

    Implemented not_String

--- quiz.py
+++ quiz.py
@@ -13,3 +13,13 @@ print has_teen(13,4,21)#Should return True
 print has_teen(17,15,16)#Should return True
 print has_teen(4,4,12)#Should return False
 print has_teen(3,4,5)#Should return False
+
+def not_string(str):
+	 if str[:1] == "not":
+		  return str + "not"
+	   else:
+		   return "not" + str
+ print not_string("not Bill")# Should return not Bill not
+ print not_string(" Bill")# Should return not Bill
+
+  
