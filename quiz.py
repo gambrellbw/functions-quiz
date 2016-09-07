4a0f08ebce0a103a9157f8542192860636f45a66
Brendan Weston Gambrell <gambrellbw@dcsdk12.org>
2016-09-07 12:40:46 -0600

    Implemented has_teen

--- quiz.py
+++ quiz.py
@@ -1,12 +1,15 @@
-	
-# TODO - write has_teen
 
-# TODO - write not_string
+def has_teen(a,b,c):
+	if a >= 13 and a <= 19:
+		return True
+	if b >= 13 and b <= 19:
+		return True
+	if c >= 13 and c <= 19:
+		return True
+	else:
+		return False
 
-# TODO - write icy_hot
-
-# TODO - write closer_to
-
-# TODO - write two_as_one
-
-# TODO - write pig_latinify
+print has_teen(13,4,21)#Should return True
+print has_teen(17,15,16)#Should return True
+print has_teen(4,4,12)#Should return False
+print has_teen(3,4,5)#Should return False
